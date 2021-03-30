1b22f5865095722d1465d48183967ab2f3afaade
Zhiwei Fang <51684958+pentilm@users.noreply.github.com>
2021-03-31 00:49:59 +0800

    update the snet daemon config

--- test_factai_service.py
+++ test_factai_service.py
@@ -15,9 +15,9 @@ if __name__ == "__main__":
 				test_flag = True
 
 		# Fact AI Service - Arithmetic
-		endpoint = input("Endpoint (localhost:{}): ".format(registry["factai_service"]["grpc"])) if not test_flag else ""
+		endpoint = input("Endpoint (195.201.197.25:{}): ".format(registry["factai_service"]["grpc"])) if not test_flag else ""
 		if endpoint == "":
-			endpoint = "localhost:{}".format(registry["factai_service"]["grpc"])
+			endpoint = "195.201.197.25:{}".format(registry["factai_service"]["grpc"])
 
 		grpc_method = "stance_classify"
 
